6c41337265159824f707e716c2ee71bff4eca53f
akhil3008 <akhil3008@gmail.com>
2022-11-29 16:15:39 +0000

    updated app name

--- ecommerce/bin/deploy.sh
+++ ecommerce/bin/deploy.sh
@@ -1,3 +1,3 @@
-sudo docker build -t talk--hoods-app--apts ../../.
-sudo docker run -it -d -p 5000:5000 -p 3306:3306 talk--hoods-app talk--hoods-app--apts
+sudo docker build -t akhil_app_talk_hoods ../../.
+sudo docker run -it -d -p 5000:5000 -p 3306:3306 akhil_app_talk_hoods
 sudo docker ps -a
