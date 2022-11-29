718207727abd0a35eae3b306521d58617f6f8ee4
akhil3008 <akhil3008@gmail.com>
2022-11-29 16:09:37 +0000

    updated database changes

--- ecommerce/bin/deploy.sh
+++ ecommerce/bin/deploy.sh
@@ -1,3 +1,3 @@
-sudo docker build -t talk--hoods-app ../../.
-sudo docker run -it -d -p 5000:5000 -p 3306:3306 talk--hoods-app
+sudo docker build -t talk--hoods-app--apts ../../.
+sudo docker run -it -d -p 5000:5000 -p 3306:3306 talk--hoods-app talk--hoods-app--apts
 sudo docker ps -a
