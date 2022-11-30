6a047a527081ea470fd4080916f6cd025864b7ed
akhil3008 <akhil3008@gmail.com>
2022-11-30 16:13:04 +0000

    updated changes in schemes

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -15,135 +15,56 @@ from ecommerce.models import *
 
 
 
-
-def is_valid(email, password):
-    # Using Flask-SQLAlchmy ORM
-    # data = User.query.with_entities(User.email, User.password).all()
-
-    # Using Raw SQL Select Query
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT email, password FROM user")
-    userData = cur.fetchall()
-    cur.close()
-
-    for row in userData:
-        if row['email'] == email and row['password'] == hashlib.md5(password.encode()).hexdigest():
-            return True
-    return False
-
-
-def getLoginUserDetails():
-    productCountinCartForGivenUser = 0
-
-    if 'email' not in session:
-        loggedIn = False
-        firstName = ''
-    else:
-        loggedIn = True
-        userid, firstName = User.query.with_entities(User.userid, User.fname).filter(
-            User.email == session['email']).first()
-
-        productCountinCart = []
-
-        # for Cart in Cart.query.filter(Cart.userId == userId).distinct(Products.productId):
-        for cart in Cart.query.filter(Cart.userid == userid).all():
-            productCountinCart.append(cart.productid)
-            productCountinCartForGivenUser = len(productCountinCart)
-
-    return (loggedIn, firstName, productCountinCartForGivenUser)
-
-def getUserId():
-
-
-    if 'email' not in session:
-        userid = 10009
-    else:
-        userid = User.query.with_entities(User.userid).filter(
-            User.email == session['email']).first()
-    userId = userid[0]
-    return userId
-
-
-
-def extractAndPersistUserDataFromForm(request):
-    password = request.form['password']
-    email = request.form['email']
-    firstName = request.form['firstName']
-    lastName = request.form['lastName']
-    address = request.form['address']
-    zipcode = request.form['zipcode']
-    city = request.form['city']
-    state = request.form['state']
-    country = request.form['country']
-    phone = request.form['phone']
-
-    user = User(fname=firstName, lname=lastName, password=hashlib.md5(password.encode()).hexdigest(), address=address,
-                city=city, state=state, country=country, zipcode=zipcode, email=email, phone=phone)
-
-    try:
-        db.session.add(user)
-        db.session.flush()
-        db.session.commit()
-    except exc.SQLAlchemyError:
-        return "Registration failed"
-    return "Registered Successfully"
-
-
-def isUserLoggedIn():
-    if 'email' not in session:
-        return False
-    else:
-        return True
-
-
-# check if user is an admin.html
-def isUserAdmin():
-    if isUserLoggedIn():
-        # ProductCategory.query.filter_by(productid=product.productid).first()
-        userId = User.query.with_entities(User.userid).filter(User.email == session['email']).first()
-        currentUser = User.query.get_or_404(userId)
-        return currentUser.isadmin
-
 class addUserForm(FlaskForm):
-    survey_start_place = StringField('Survey Place:', validators=[DataRequired()])
+    survey_start_place = StringField('Survey Place:')
     name = StringField('Name:', validators=[DataRequired()])
-    user_mobile_number = IntegerField('Mobile Number:', validators=[DataRequired()])
-    email = StringField('Email:', validators=[DataRequired()])
-    village_name = StringField('Village Name:', validators=[DataRequired()])
-    constituency = StringField('Constituency:', validators=[DataRequired()])
+    user_mobile_number = StringField('Mobile Number:')
+    email = StringField('Email:')
+    village_name = StringField('Village Name:')
+    constituency = StringField('Constituency:')
+    state = SelectField('State:', choices=[(1, 'Telangana'), (2, 'Andhra Pradesh')])
     category = SelectField('Caste:', coerce=int, id='select_category')
-    occupation = StringField('Occupation:', validators=[DataRequired()])
-    supporting_party = StringField('Supporting Party:', validators=[DataRequired()])
-    roads = StringField('Roads:', validators=[DataRequired()])
-    drinage = StringField('Drinage:', validators=[DataRequired()])
-    grave_yards = StringField('Grave Yards:', validators=[DataRequired()])
-    water = StringField('Water:', validators=[DataRequired()])
-    houses = StringField('Houses:', validators=[DataRequired()])
-    land_allotments = StringField('Land Allotments:', validators=[DataRequired()])
-    current_mla = StringField('Current MLA:', validators=[DataRequired()])
-    number_of_visits_current = IntegerField('Number of Visits:', validators=[DataRequired()])
-    ex_mla = StringField('Ex MLA:', validators=[DataRequired()])
-    number_of_visits_ex = IntegerField('Number of Visits Ex MLA:', validators=[DataRequired()])
-    future_contestents = StringField('Future Contestents:', validators=[DataRequired()])
-    expectations = StringField('Expectations:', validators=[DataRequired()])
-    head_of_each_caste_name = StringField('Head of each Caste Name:', validators=[DataRequired()])
-    caste_head_mobile_number = IntegerField('Caste Head Mobile Number:', validators=[DataRequired()])
+    occupation = StringField('Occupation:')
+    aadharcard = StringField('Aadhar Card:')
+    rationcard = SelectField('Ration Card:', choices=[(1, 'Yes'), (0, 'No')])
+    rationcard_update = StringField('If No, Provide details of removal/ Not received')
+    supporting_party = StringField('Supporting Party:')
+    roads = StringField('Roads:')
+    drinage = StringField('Drinage:')
+    grave_yards = StringField('Grave Yards:')
+    water = StringField('Water:')
+    houses = StringField('Houses:')
+    land_allotments = StringField('Land Allotments:')
+    current_mla = StringField('Current MLA:')
+    number_of_visits_current = StringField('Number of Visits:')
+    issues = StringField('Issues:')
+    ex_mla = StringField('Ex MLA:')
+    number_of_visits_ex = StringField('Number of Visits Ex MLA:')
+    future_contestents = StringField('Future Contestents:')
+    expectations = StringField('Expectations:')
+    head_of_each_caste_name = StringField('Head of each Caste Name:')
+    caste_head_mobile_number = StringField('Caste Head Mobile Number:')
     submit = SubmitField('Save')
 
 class addschemeForm(FlaskForm):
-    scheme1 = SelectField('Annadata Sukhibava Eligible:', choices=[(1, 'Yes'), (2, 'No')])
-    scheme1_received = SelectField('Received:', choices=[(1, 'Yes'), (2, 'No')])
+    scheme1 = SelectField('Annadata Sukhibava:', choices=[(1, 'Yes'), (0, 'No')])
+    scheme1_received = SelectField('Received:', choices=[(1, 'Yes'), (0, 'No')])
     scheme1_benefits = StringField('Benefits:')
-    scheme2 = SelectField('Pasupu Kunkuma Eligible:', choices=[(1, 'Yes'), (2, 'No')])
-    scheme2_received = SelectField('Received:', choices=[(1, 'Yes'), (2, 'No')])
+    scheme1_benefits_other = StringField('Benefits_other:')
+    scheme2 = SelectField('Pasupu Kunkuma Eligible:', choices=[(1, 'Yes'), (0, 'No')])
+    scheme2_received = SelectField('Received:', choices=[(1, 'Yes'), (0, 'No')])
     scheme2_benefits = StringField('Benefits:')
-    scheme3 = SelectField('Pensioners Eligible:', choices=[(1, 'Yes'), (2, 'No')])
-    scheme3_received = SelectField('Received:', choices=[(1, 'Yes'), (2, 'No')])
+    scheme2_benefits_other = StringField('Benefits_other:')
+    scheme3 = SelectField('Pensioners Eligible:', choices=[(1, 'Yes'), (0, 'No')])
+    scheme3_received = SelectField('Received:', choices=[(1, 'Yes'), (0, 'No')])
     scheme3_benefits = StringField('Benefits:')
-    scheme4 = SelectField('Ammaki Vandanam Eligible:', choices=[(1, 'Yes'), (2, 'No')])
-    scheme4_received = SelectField('Received:', choices=[(1, 'Yes'), (2, 'No')])
+    scheme3_benefits_other = StringField('Benefits_other:')
+    scheme4 = SelectField('Ammaki Vandanam Eligible:', choices=[(1, 'Yes'), (0, 'No')])
+    scheme4_received = SelectField('Received:', choices=[(1, 'Yes'), (0, 'No')])
     scheme4_benefits = StringField('Benefits:')
-    scheme5 = SelectField('Unemployment Eligible:', choices=[(1, 'Yes'), (2, 'No')])
-    scheme5_received = SelectField('Received:', choices=[(1, 'Yes'), (2, 'No')])
+    scheme4_benefits_other = StringField('Benefits_other:')
+    scheme5 = SelectField('Unemployment Eligible:', choices=[(1, 'Yes'), (0, 'No')])
+    scheme5_received = SelectField('Received:', choices=[(1, 'Yes'), (0, 'No')])
     scheme5_benefits = StringField('Benefits:')
+    scheme5_benefits_other = StringField('Benefits_other:')
     submit = SubmitField('Save')
--- ecommerce/models.py
+++ ecommerce/models.py
@@ -10,6 +10,7 @@ class User(db.Model):
     fname = db.Column(db.String(20), nullable=False)
     lname = db.Column(db.String(120), nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    active = db.Column(db.Integer, default=1)
     address = db.Column(db.String(100), unique=False, nullable=False)
     city = db.Column(db.String(100), unique=False, nullable=False)
     state = db.Column(db.String(100), unique=False, nullable=False)
@@ -17,6 +18,8 @@ class User(db.Model):
     zipcode = db.Column(db.String(100), unique=False, nullable=False)
     email = db.Column(db.String(120), nullable=False)
     phone = db.Column(db.String(20), nullable=False)
+    isadmin = db.Column(db.Integer, default=0)
+    created_on = db.Column(db.String(25))
 
     def __repr__(self):
         return f"User('{self.fname}', '{self.lname}'), '{self.password}', " \
@@ -24,42 +27,21 @@ class User(db.Model):
                f"'{self.zipcode}','{self.email}','{self.phone}')"
 
 
-class Category(db.Model):
-    __table_args__ = {'extend_existing': True}
-    categoryid = db.Column(db.Integer, primary_key=True)
-    category_name = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-    def __repr__(self):
-        return f"Category('{self.categoryid}', '{self.category_name}')"
-
-
-class Product(db.Model):
-
-    __table_args__ = {'extend_existing': True}
-    productid = db.Column(db.Integer, primary_key=True)
-    sku = db.Column(db.String(50), nullable=False)
-    product_name = db.Column(db.String(1000), nullable=False)
-    description = db.Column(db.String(1000), nullable=False)
-    image = db.Column(db.String(1000), nullable=False)
-    stock = db.Column(db.Integer, nullable=False)
-    regular_price = db.Column(db.DECIMAL)
-    discounted_price = db.Column(db.DECIMAL)
-    product_rating = db.Column(db.DECIMAL)
-    product_review = db.Column(db.String(100), nullable=True)
-    sub_product_id = db.Column(db.String(200), nullable=True)
-    brand = db.Column(db.String(200), nullable=True)
-    weight = db.Column(db.String(200), nullable=True)
-
 class user_talks(db.Model):
     __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
     survey_start_place = db.Column(db.String(50))
     name = db.Column(db.String(50))
-    user_mobile_number = db.Column(db.Integer, primary_key=True)
+    user_mobile_number = db.Column(db.String(12))
     email = db.Column(db.String(100))
     village_name = db.Column(db.String(100))
+    constituency = db.Column(db.String(100))
+    state = db.Column(db.String(100))
     caste = db.Column(db.String(1000))
     occupation = db.Column(db.String(100))
+    aadharcard = db.Column(db.String(100))
+    rationcard = db.Column(db.String(100))
+    rationcard_update = db.Column(db.String(100))
     supporting_party = db.Column(db.String(100))
     roads = db.Column(db.String(100))
     drinage = db.Column(db.String(100))
@@ -68,16 +50,25 @@ class user_talks(db.Model):
     houses = db.Column(db.String(100))
     land_allotments = db.Column(db.String(100))
     current_mla = db.Column(db.String(100))
-    number_of_visits_current = db.Column(db.Integer)
+    number_of_visits_current = db.Column(db.Integer, default=0)
+    issues = db.Column(db.String(1000))
     ex_mla = db.Column(db.String(100))
-    number_of_visits_ex = db.Column(db.Integer)
+    number_of_visits_ex = db.Column(db.Integer, default=0)
     future_contestents = db.Column(db.String(200))
     expectations = db.Column(db.String(500))
     head_of_each_caste_name = db.Column(db.String(200))
-    caste_head_mobile_number = db.Column(db.Integer)
+    caste_head_mobile_number = db.Column(db.String(12))
+    created_on = db.Column(db.DateTime)
 
     def __repr__(self):
-        return f"Product('{self.productid}','{self.product_name}','{self.description}', '{self.image}',  '{self.stock}','{self.regular_price}', '{self.discounted_price}', '{self.sub_product_id}', '{self.brand}','{self.weight}')"
+        return f"user_talks('{self.survey_start_place}','{self.name}','{self.user_mobile_number}', \
+        '{self.email}',  '{self.village_name}','{self.constituency}', '{self.caste}', \
+        '{self.occupation}','{self.aadharcard}','{self.rationcard}','{self.rationcard_update}', \
+        '{self.supporting_party}', '{self.roads}','{self.drinage}','{self.grave_yards}',\
+        '{self.water}','{self.houses}','{self.land_allotments}','{self.current_mla}', \
+        '{self.number_of_visits_current}','{self.issues}','{self.ex_mla}','{self.number_of_visits_ex}', \
+        '{self.future_contestents}','{self.expectations}','{self.head_of_each_caste_name}',\
+        '{self.caste_head_mobile_number}','{self.created_on}')"
 
 class scheme_benefits(db.Model):
     __table_args__ = {'extend_existing': True}
@@ -87,72 +78,15 @@ class scheme_benefits(db.Model):
     eligible = db.Column(db.String(3))
     received = db.Column(db.String(3))
     benefits = db.Column(db.String(1000))
+    benefits_other = db.Column(db.String(1000))
 
     def __repr__(self):
-        return f"Product('{self.participant_name}','{self.scheme}','{self.eligible}', '{self.received}',  '{self.benefits}')"
+        return f"scheme_benefits('{self.participant_name}','{self.scheme}','{self.eligible}', '{self.received}',  '{self.benefits}')"
 
-class ProductCategory(db.Model):
+class Caste(db.Model):
     __table_args__ = {'extend_existing': True}
-    categoryid = db.Column(db.Integer, db.ForeignKey('category.categoryid'), nullable=False, primary_key=True)
-    productid = db.Column(db.Integer, db.ForeignKey('product.productid'), nullable=False, primary_key=True)
-    created_on = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    casteid = db.Column(db.Integer, primary_key=True)
+    caste_name = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
-        return f"Product('{self.categoryid}', '{self.productid}')"
-
-class SubProducts(db.Model):
-    __table_args__ = {'extend_existing': True}
-    product_id = db.Column(db.Integer, nullable=False,  primary_key=True)
-    sub_product_id = db.Column(db.Integer, nullable=False ,  primary_key=True)
-    weights = db.Column(db.String(64), nullable=False)
-
-    def __repr__(self):
-        return f"SubProduct('{self.product_id}', '{self.sub_product_id}', '{self.weights}')"
-
-class Cart(db.Model):
-    __table_args__ = {'extend_existing': True}
-    userid = db.Column(db.Integer, db.ForeignKey('user.userid'), nullable=False, primary_key=True)
-    productid = db.Column(db.Integer, db.ForeignKey('product.productid'), nullable=False, primary_key=True)
-    quantity = db.Column(db.Integer, nullable=False)
-    subproductid = db.Column(db.String(200), nullable=False)
-
-    def __repr__(self):
-        return f"Cart('{self.userid}', '{self.productid}, '{self.quantity}', '{self.subproductid}')"
-
-
-class Order(db.Model):
-    __table_args__ = {'extend_existing': True}
-    orderid = db.Column(db.Integer, primary_key=True)
-    order_date = db.Column(db.DateTime, nullable=False)
-    total_price = db.Column(db.DECIMAL, nullable=False)
-    userid = db.Column(db.Integer, db.ForeignKey('user.userid'), nullable=False, primary_key=True)
-
-    def __repr__(self):
-        return f"Order('{self.orderid}', '{self.order_date}','{self.total_price}','{self.userid}'')"
-#
-class OrderedProduct(db.Model):
-    __tablename__ = "ordered_details"
-    __table_args__ = {'extend_existing': True}
-    ordproductid = db.Column(db.Integer, primary_key=True)
-    orderid = db.Column(db.Integer,db.ForeignKey('order.orderid'), nullable=False)
-    productid = db.Column(db.Integer,db.ForeignKey('product.productid'), nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
-    subproductid = db.Column(db.Integer, nullable=False)
-
-    def __repr__(self):
-        return f"Order('{self.ordproductid}', '{self.orderid}','{self.productid}','{self.quantity}','{self.subproductid}')"
-
-
-
-class SaleTransaction(db.Model):
-    __table_args__ = {'extend_existing': True}
-    transactionid = db.Column(db.Integer, primary_key=True)
-    orderid = db.Column(db.Integer,db.ForeignKey('order.orderid'), nullable=False)
-    transaction_date = db.Column(db.DateTime,nullable=False)
-    amount = db.Column(db.DECIMAL, nullable=False)
-    cc_number=db.Column(db.String(50), nullable=False)
-    cc_type = db.Column(db.String(50), nullable=False)
-    response = db.Column(db.String(50), nullable=False)
-
-    def __repr__(self):
-        return f"Order('{self.transactionid}', '{self.orderid}','{self.transactiondate}','{self.amount}', '{self.cc_number}','{self.cc_type}','{self.response}')"
+        return f"Category('{self.casteid}', '{self.caste_name}')"
