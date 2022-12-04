03b856c68183ccf4f18c772ef01e40757188210c
akhil3008 <akhil3008@gmail.com>
2022-12-04 14:38:08 +0000

    updated image path

--- ecommerce/routes.py
+++ ecommerce/routes.py
@@ -82,8 +82,9 @@ def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    
-    picture_path = os.path.join(app.root_path, 'static/uploads', picture_fn)
+
+    picture_path = os.path.join('/tmp/static/uploads/', picture_fn)
+    # picture_path = os.path.join(app.root_path, 'static/uploads', picture_fn)
     print(picture_path)
 
     output_size = (250, 250)
